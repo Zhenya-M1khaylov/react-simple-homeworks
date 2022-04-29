14a92122d80a9ec424662122e01976a58e64eb69
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-04-29 23:05:49 +0300

    add NavLinks and routes

--- my-app/src/p2-homeworks/h2/Affair.tsx
+++ my-app/src/p2-homeworks/h2/Affair.tsx
@@ -6,16 +6,23 @@ import s from './Affairs.module.css'
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: (_id: number)=>void
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = (id: number) => {props.deleteAffairCallback(id)}
+    const deleteCallback = (id: number) => {
+        props.deleteAffairCallback(id)
+    }
 
     return (
         <div>
-            {props.affair.name}
-            <button onClick={() => deleteCallback(props.affair._id)}>X</button>
+            <tr>
+                <td>{props.affair.name}</td>
+                <td>{props.affair.priority}</td>
+                <td>
+                    <button className={s.buttons} onClick={() => deleteCallback(props.affair._id)}>X</button>
+                </td>
+            </tr>
         </div>
     )
 }
--- my-app/src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ my-app/src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
+import './Header.css'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            {/*// add NavLinks*/}
+            {/*<NavLink*/}
+            {/*    to={'/'}*/}
+            {/*    // style={(params)=> {*/}
+            {/*    //     return {color: params.isActive ? 'lime' : 'black'}*/}
+            {/*    // }}*/}
+            {/*    className={({isActive})=>isActive ? 'act' : 'def'}*/}
+            {/*></NavLink>*/}
+            <NavLink
+                to={'/pre-junior'}
+                // style={(params)=> {
+                //     return {color: params.isActive ? 'lime' : 'black'}
+                // }}
+                className={({isActive})=>isActive ? 'act' : 'def'}
+            >
+                pre-junior
+            </NavLink>
+            <NavLink
+                to={'/junior'}
+                // style={(params)=> {
+                //     return {color: params.isActive ? 'lime' : 'black'}
+                // }}
+                className={({isActive})=>isActive ? 'act' : 'def'}
+            >
+                junior
+            </NavLink>
+            <NavLink
+                to={'/juniorPlus'}
+                // style={(params)=> {
+                //     return {color: params.isActive ? 'lime' : 'black'}
+                // }}
+                className={({isActive})=>isActive ? 'act' : 'def'}
+            >
+                juniorPlus
+            </NavLink>
         </div>
     )
 }
